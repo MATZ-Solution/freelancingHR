69cd8a4e373fc02c34b5b559ba7dc9649ef938be
umair <umairnazakat2222@gmail.com>
2024-04-20 13:00:34 +0500

    Update email credentials

--- Backend/helper/googleAuth.js
+++ Backend/helper/googleAuth.js
@@ -1,47 +1,3 @@
-// const { google } = require('googleapis');
-// // const dotenv = require('dotenv');
-// const nodemailer=require('nodemailer')
-// // const constants=require('../../constants')
-// // dotenv.config()
-// const constants=process.env
-// const OAuth2 = google.auth.OAuth2;
-// exports.createTransporter = async () => {
-//     const oauth2Client = new OAuth2(
-//       constants.CLIENT_ID,
-//       constants.CLIENT_SECRET,
-//       "https://developers.google.com/oauthplayground"
-//     );
-  
-//     oauth2Client.setCredentials({
-//       refresh_token: constants.REFRESH_TOKEN
-//     });
-  
-//     const accessToken = await new Promise((resolve, reject) => {
-//       oauth2Client.getAccessToken((err, token) => {
-//         if (err) {
-//           reject("Failed to create access token :(");
-//         }
-//         resolve(token);
-//       });
-//     });
-  
-//     const transporter = nodemailer.createTransport({
-//       service: "gmail",
-//       auth: {
-//         type: "OAuth2",
-//         user: constants.EMAIL_HOST,
-//         accessToken,
-//         clientId: constants.CLIENT_ID,
-//         clientSecret: constants.CLIENT_SECRET,
-//         refreshToken: constants.REFRESH_TOKEN
-//       }
-//     });
-  
-//     return transporter;
-//   };
-
-
-
 const { google } = require('googleapis');
 // const dotenv = require('dotenv');
 const nodemailer=require('nodemailer')
@@ -50,44 +6,47 @@ const nodemailer=require('nodemailer')
 const constants=process.env
 const OAuth2 = google.auth.OAuth2;
 // const { google } = require('googleapis');
+// console.log(constants.CLIENT_ID);
 exports.createTransporter = async () => {
-    const oauth2Client = new google.auth.OAuth2(
-      constants.CLIENT_ID,
-      constants.CLIENT_SECRET
-      // ,
-      // "https://developers.google.com/oauthplayground"
+    const oauth2Client = new OAuth2(
+      // constants.CLIENT_ID,
+      "148416344022-n0ur9iaiaoos4nvig9jhsfubjdrdsu90.apps.googleusercontent.com",
+      // constants.CLIENT_SECRET,
+      "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",
+      // constants.AUTH_URL
+      "https://developers.google.com/oauthplayground"
     );
-  
-    // oauth2Client.setCredentials({
-    //   refresh_token: constants.REFRESH_TOKEN
-    // });
-
     oauth2Client.setCredentials({
-      refresh_token: "1//04r1xQQrd-pBnCgYIARAAGAQSNwF-L9IrmOt54IjVjyKzqAoun_t681PzQEhnM5Q3DeUpaVMUZ24DCqhveAwnqQiMaHFTgYAMqLM"
+      // refresh_token: constants.REFRESH_TOKEN
+      refresh_token: "1//04zYh-Z0O-s_SCgYIARAAGAQSNwF-L9Ir8OalRilhcVDdf1siseaXVmxbUuE6SexH5lRXhHWJggJ9VFGpHq46hX9RjFbL8wV4hHE"
     });
     
   
-    // const accessToken = await new Promise((resolve, reject) => {
-    //   oauth2Client.getAccessToken((err, token) => {
-    //     if (err) {
-    //       reject("Failed to create access token :(" + err);
-    //     }
-    //     resolve(token);
-    //   });
-    // });
+    const accessToken = await new Promise((resolve, reject) => {
+      oauth2Client.getAccessToken((err, token) => {
+        if (err) {
+          reject("Failed to create access token :(" + err);
+        }
+        resolve(token);
+      });
+    });
   
-    const accessToken = await oauth2Client.getAccessToken();
+    // const accessToken = await oauth2Client.getAccessToken();
 
 
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
         type: "OAuth2",
-        user: constants.EMAIL_HOST,
+        // user: constants.EMAIL_HOST,
+        user: 'freelancinghr0@gmail.com',
         accessToken,
-        clientId: constants.CLIENT_ID,
-        clientSecret: constants.CLIENT_SECRET,
-        refreshToken: constants.REFRESH_TOKEN
+        // clientId: constants.CLIENT_ID,
+        // clientSecret: constants.CLIENT_SECRET,
+        // refreshToken: constants.REFRESH_TOKEN
+        clientId: "148416344022-n0ur9iaiaoos4nvig9jhsfubjdrdsu90.apps.googleusercontent.com",
+        clientSecret: "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",
+        refreshToken: "1//04zYh-Z0O-s_SCgYIARAAGAQSNwF-L9Ir8OalRilhcVDdf1siseaXVmxbUuE6SexH5lRXhHWJggJ9VFGpHq46hX9RjFbL8wV4hHE"
       }
     });
   
